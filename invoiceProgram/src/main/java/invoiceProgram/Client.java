7bf488822008787a379fd043071ed63ace73c05a
Going33 <going33@wp.pl>
2023-02-21 18:17:22 +0100

    Update Client.java

--- invoiceProgram/src/main/java/invoiceProgram/Client.java
+++ invoiceProgram/src/main/java/invoiceProgram/Client.java
@@ -1,9 +1,57 @@
 package invoiceProgram;
 
-public class SellerBuyer {
-//dupa to jest
-	public SellerBuyer() {
-		// TODO Auto-generated constructor stub
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Client implements invoiceParticipantsInterface {
+
+	private Map<String, String> ClientMap = new TreeMap<String, String>();
+	private Set<String> keys = ClientMap.keySet(); // The set of keys in the map.
+	private Iterator<String> keyIter = keys.iterator();
+	
+	
+	
+	//////////
+	
+	public Map<String, String> getMapBuyer()
+	{
+		return this.ClientMap;
 	}
+	
+
+	//////////
+	
+	@Override
+	public void putInInfos(String string1, String string2) {
+		
+		ClientMap.put(string1, string2);
+	}
+	@Override
+	public void displayInInfos() {
+		for (Map.Entry<String, String> set : ClientMap.entrySet()) {
+			// Printing all elements of a Map
+			System.out.println(set.getKey() + " " + set.getValue());
+		}
+
+
+	}
+	
+	@Override
+	public void deleteDuplicate() {
+		while (keyIter.hasNext()) {
+			String key = keyIter.next();
+			String value = ClientMap.get(key);
+			ClientMap.put(value, key);
+		}
+		
+	}
+	@Override
+	public void clearTreeMap() {
+		ClientMap.clear();
+		
+	}
+
 
 }
