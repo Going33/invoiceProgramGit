142a0600b0b1fcbddff8036119b6dcb9e195c8e0
Going33 <going33@wp.pl>
2023-03-02 01:47:27 +0100

    setDate added

--- invoiceProgram/src/main/java/invoiceProgram/saveTextFile.java
+++ invoiceProgram/src/main/java/invoiceProgram/saveTextFile.java
@@ -5,6 +5,7 @@ import java.io.File; // Import the File class
 import java.io.FileWriter;
 import java.io.IOException; // Import the IOException class to handle errors
 import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.Map;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -17,12 +18,9 @@ public class saveTextFile {
 
 	// tworzenie pliku
 	// private File myObj = new File("C:\\Users\\Bartek\\Desktop\\TEST\\test.txt");
-	final private String absPathTxt = System.getProperty("user.dir") + "\\test.txt";
-	final private String absPathPDF = System.getProperty("user.dir") + "\\testPDF.pdf";
-	final private File myObj = new File(absPathTxt);
+	final private String absPathPDF = System.getProperty("user.dir") + "\\Invoice_"+LocalDate.now()+".pdf";
 	final private File myObjPDF = new File(absPathPDF);
 
-	private BufferedWriter bf;
 	private boolean flagDelete;
 	private boolean flagCheck;
 	private int fontSizeValue = 12;
@@ -36,7 +34,7 @@ public class saveTextFile {
 			// creating new file
 			if (myObjPDF.createNewFile()) {
 
-				System.out.println("File created: " + myObj.getName());
+				System.out.println("File created: " + myObjPDF.getName());
 				System.out.println(System.getProperty("user.dir"));
 				// create a file
 
@@ -45,14 +43,14 @@ public class saveTextFile {
 				// PDPage page = document.getPage(0);
 
 				createAFilePDF(allDataMap, document, document.getPage(0),
-						new PDPageContentStream(document, document.getPage(0)), 5, 780);
+						new PDPageContentStream(document, document.getPage(0)), 5, 750,true);
 				System.out.println("PDF file created.");
 
 			} else {
 
 				PDDocument document = PDDocument.load(myObjPDF);
 				createAFilePDF(allDataMap, document, document.getPage(0), new PDPageContentStream(document,
-						document.getPage(0), PDPageContentStream.AppendMode.APPEND, true), 350, 780);
+						document.getPage(0), PDPageContentStream.AppendMode.APPEND, true), 350, 750,false);
 
 				System.out.println("File already exists. Append new text to existing file.");
 				// }
@@ -67,14 +65,25 @@ public class saveTextFile {
 
 	/////////////////////////////// to optmize//////////////////////////////
 	public void createAFilePDF(Object object, PDDocument document, PDPage page, PDPageContentStream contentStream,
-			int tX, int tY) throws IOException {
+			int tX, int tY,boolean setDate) throws IOException {
 
 		try {
 			System.out.println("\n");
 			contentStream.beginText();
+
 			// Setting the font to the Content stream
 			contentStream.setFont(PDType1Font.TIMES_ROMAN, fontSizeValue);
-			contentStream.newLineAtOffset(tX, tY);
+			
+				contentStream.newLineAtOffset(tX, tY);
+				if(setDate)
+				{
+					contentStream.newLineAtOffset(tX+530, 30);
+					contentStream.showText(LocalDate.now().toString());
+					contentStream.newLineAtOffset(tX-530, -30);
+				}
+						
+
+
 			for (Map.Entry<String, String> set : ((Map<String, String>) object).entrySet()) {
 				contentStream.showText(set.getKey().toString() + " " + set.getValue().toString());
 				contentStream.newLineAtOffset(0, -20);
@@ -93,30 +102,30 @@ public class saveTextFile {
 	}
 
 /////////////////////////////// to optmize//////////////////////////////
-	public void createAFile(Object object) {
-// write inputs to file if file dosent exist
-		try {
-			System.out.println("\n");
-			bf = new BufferedWriter(new FileWriter(myObj));
-
-			for (Map.Entry<String, String> set : ((Map<String, String>) object).entrySet()) {
-				bf.write(set.getKey() + " " + set.getValue());
-				bf.newLine();
-			}
-
-			bf.flush();
-			bf.close();
-
-		} catch (IOException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-	}
+//	public void createAFile(Object object) {
+//// write inputs to file if file dosent exist
+//		try {
+//			System.out.println("\n");
+//			bf = new BufferedWriter(new FileWriter(myObj));
+//
+//			for (Map.Entry<String, String> set : ((Map<String, String>) object).entrySet()) {
+//				bf.write(set.getKey() + " " + set.getValue());
+//				bf.newLine();
+//			}
+//
+//			bf.flush();
+//			bf.close();
+//
+//		} catch (IOException e) {
+//			System.out.println("An error occurred.");
+//			e.printStackTrace();
+//		}
+//	}
 
 /////////////////////////////////////////////////////////////////////
 	public boolean deleteAFIle() {
 
-		if (myObj.delete()) {
+		if (myObjPDF.delete()) {
 			System.out.println("File deleted successfully");
 			flagDelete = true;
 		} else {
@@ -127,16 +136,16 @@ public class saveTextFile {
 	}
 
 	////// TEST_METHOD///////
-	public boolean checkIfAFIleIsAlreadyExisting() {
-		if (myObj.exists()) {
-			System.out.println("File already exists. Cannot create a new one.");
-			flagCheck = false;
-		} else {
-			System.out.println("Confirmed. Successfully wrote to the file.");
-			flagCheck = true;
-		}
-		return flagCheck;
-	}
+//	public boolean checkIfAFIleIsAlreadyExisting() {
+//		if (myObj.exists()) {
+//			System.out.println("File already exists. Cannot create a new one.");
+//			flagCheck = false;
+//		} else {
+//			System.out.println("Confirmed. Successfully wrote to the file.");
+//			flagCheck = true;
+//		}
+//		return flagCheck;
+//	}
 
 	public boolean checkIfAFIleIsAlreadyExistingPDF() {
 		if (myObjPDF.exists()) {
